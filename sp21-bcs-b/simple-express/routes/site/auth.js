707a454e9e8506c2994a1fdfba5c258448595a12
Usman Akram <usman.akram@gmail.com>
2023-11-13 16:57:38 +0500

    b section auth

--- sp21-bcs-b/simple-express/routes/site/auth.js
+++ sp21-bcs-b/simple-express/routes/site/auth.js
@@ -5,6 +5,28 @@ router.get("/register", (req, res) => {
   //   return res.send(req.query);
   res.render("auth/register");
 });
+router.get("/login", (req, res) => {
+  //   return res.send(req.query);
+  res.render("auth/login");
+});
+router.get("/logout", (req, res) => {
+  //   return res.send(req.query);
+  req.session.user = null;
+  res.redirect("/login");
+});
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  let user = await User.findOne({ email });
+  if (!user) return res.redirect("/register");
+  const bcrypt = require("bcryptjs");
+  console.log(req.body);
+  const isValid = await bcrypt.compare(password, user.password);
+  if (isValid) {
+    req.session.user = user;
+    return res.redirect("/");
+  } else return res.redirect("/login");
+  // res.status(400).send({ isValid });
+});
 
 router.post("/register", async (req, res) => {
   let user = new User(req.body);
--- sp21-bcs-b/simple-express/server.js
+++ sp21-bcs-b/simple-express/server.js
@@ -4,6 +4,8 @@
 // run this command once
 //npm i nodemon -g
 let express = require("express");
+let cookieParser = require("cookie-parser");
+var session = require("express-session");
 var expressLayouts = require("express-ejs-layouts");
 let app = express();
 app.use(express.static("public"));
@@ -12,26 +14,41 @@ app.set("view engine", "ejs");
 app.use(expressLayouts);
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(cookieParser());
+app.use(session({ secret: "Shh, its a secret!" }));
+app.use(require("./middlewares/common"));
 // app.get("/", function (req, res) {
 //   res.send("<h1>Home Page</h1>");
 // });
-app.get("/products", function (req, res) {
+
+const maintenance = require("./middlewares/maintenance");
+const logger = require("./middlewares/logger");
+const sessionauth = require("./middlewares/sessionauth");
+// app.use(logger);
+app.get("/products", logger, function (req, res) {
   let products = [
     { title: "Marker", price: 700 },
     { title: "Pen", price: 300 },
     { title: "rubber", price: 100 },
   ];
+  // app.use(maintenance);
   res.render("products/list", {
     pageTitle: "This is products list page",
     products: products,
   });
 });
-app.get("/contact-us", function (req, res) {
+app.get("/contact-us", sessionauth, function (req, res, next) {
   res.render("contact-us");
+  // next();
 });
 
-app.get("/test", (req, res) => {
-  res.send("Hello Class");
+app.get("/views", (req, res) => {
+  let visits = req.cookies.visits;
+
+  if (!visits) visits = 1;
+  else visits = visits + 1;
+  res.cookie("visits", visits);
+  res.send({ visits });
 });
 const Car = require("./models/car");
 
@@ -47,6 +64,7 @@ app.get("/", function (req, res) {
   res.render("landing-page", {});
 });
 const mongoose = require("mongoose");
+const { cookie } = require("express/lib/response");
 mongoose
   .connect("mongodb://localhost/sp21-bcs-b", { useNewUrlParser: true })
   .then(() => console.log("Connected to Mongo...."))
