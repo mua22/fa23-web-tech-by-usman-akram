78fe466c36235d1ab8b155c6771a02d45f28ec36
Usman Akram <usman.akram@gmail.com>
2023-11-16 16:24:11 +0500

    a section Auth

--- sp21-bcs-a/simple-express/routes/site/auth.js
+++ sp21-bcs-a/simple-express/routes/site/auth.js
@@ -2,6 +2,18 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 let router = express.Router();
 let User = require("../../models/user");
+router.get("/login", (req, res) => {
+  res.render("auth/login");
+});
+router.post("/login", async (req, res) => {
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.redirect("/login");
+  let valid = await bcrypt.compare(req.body.password, user.password);
+  if (!valid) return res.redirect("/login");
+  req.session.user = user; //actually logging in
+  return res.redirect("/");
+});
+
 router.get("/register", (req, res) => {
   res.render("auth/register");
 });
--- sp21-bcs-a/simple-express/server.js
+++ sp21-bcs-a/simple-express/server.js
@@ -12,18 +12,32 @@ server.use(express.json());
 server.use(express.urlencoded());
 server.use(expressLayouts);
 
+var cookieParser = require("cookie-parser");
+server.use(cookieParser());
+var session = require("express-session");
+server.use(session({ secret: "Shh, its a secret!" }));
+
+const sessionAuth = require("./middlewares/sessionAuth");
+const logger = require("./middlewares/logger");
+const maintenance = require("./middlewares/maintenance");
+server.use(logger);
+// server.use(maintenance);
 /**
  * First parameter is uri
  * Secind Parameter is a call =back function whihc will be called when a request to / with get method will be received
  *
  */
-server.get("/hobbies", function (req, res) {
+server.get("/hobbies", maintenance, function (req, res) {
   res.render("hobbies");
 });
-server.get("/skills", function (req, res) {
+server.get("/skills", sessionAuth, function (req, res) {
   res.render("skills");
 });
 
+server.get("/page-views", (req, res) => {
+  // res.cookie("location", "Lahore");
+  return res.send(req.cookies);
+});
 server.get("/posts/:month/:day", function (req, res) {
   return res.send(req.params);
 });
@@ -35,6 +49,7 @@ server.get("/", function (req, res) {
   res.render("homepage");
 });
 const mongoose = require("mongoose");
+const { append } = require("express/lib/response");
 //mongodb+srv://<username>:<password>@cluster0.vidd3.mongodb.net/
 mongoose
   .connect("mongodb://localhost/sp21-bcs-a", { useNewUrlParser: true })
